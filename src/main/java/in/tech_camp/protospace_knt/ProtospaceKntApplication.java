64309f1532cdeca305e3b7c335edccaf198b6c64
rikoko-web <e.katayama197@kntct.com>
2026-06-15 08:46:28 +0900

    Initial commit

---
+++ src/main/java/in/tech_camp/protospace_knt/ProtospaceKntApplication.java
@@ -0,0 +1,13 @@
+package in.tech_camp.protospace_knt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtospaceKntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtospaceKntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/tech_camp/protospace_knt/ProtospaceKntApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_camp.protospace_knt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtospaceKntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
