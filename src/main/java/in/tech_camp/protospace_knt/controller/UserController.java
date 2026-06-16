d296d1be9ee8eb6213b32cf1c3732dcbb8223b75
erichaki <e.yamazawa185@kntct.com>
2026-06-16 14:17:04 +0900

    フロント作成⑦

--- src/main/java/in/tech_camp/protospace_knt/controller/UserController.java
+++ src/main/java/in/tech_camp/protospace_knt/controller/UserController.java
@@ -40,7 +40,7 @@ public class UserController {
     @GetMapping("/signup")
     public String showSignupForm(Model model) {
         model.addAttribute("userEntity", new UserEntity());
-        return "users/signup"; 
+        return "users/signUp"; 
     }
 
     // 【新規登録のボタンが押された時の処理】
