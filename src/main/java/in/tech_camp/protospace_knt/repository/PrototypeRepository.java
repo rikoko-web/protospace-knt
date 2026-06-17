e86d254243fbd8c4c1ce9199a74b670011b5962c
rikoko-web <e.katayama197@kntct.com>
2026-06-17 20:46:53 +0900

    ooさんへ移行はできる

--- src/main/java/in/tech_camp/protospace_knt/repository/PrototypeRepository.java
+++ src/main/java/in/tech_camp/protospace_knt/repository/PrototypeRepository.java
@@ -1,11 +1,11 @@
 package in.tech_camp.protospace_knt.repository;
 
-import java.util.List; // ★これが必要
+import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result; // ★これが必要
-import org.apache.ibatis.annotations.Results; // ★これが必要
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import in.tech_camp.protospace_knt.entity.PrototypeEntity;
