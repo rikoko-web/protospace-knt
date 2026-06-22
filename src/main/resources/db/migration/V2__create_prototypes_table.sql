1cb94202e1c1d1a4391660150463a52646f29605
erichaki <e.yamazawa185@kntct.com>
2026-06-22 11:40:35 +0900

    dbとCSSの修正

---
+++ src/main/resources/db/migration/V2__create_prototypes_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE prototypes (
+    id SERIAL NOT NULL,
+    title VARCHAR(256) NOT NULL,
+    catch_copy VARCHAR(512),
+    concept TEXT,
+    image VARCHAR(256),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    user_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
--- src/main/resources/db/migration/V3__create_comments_table.sql
+++ src/main/resources/db/migration/V3__create_comments_table.sql
@@ -3,5 +3,7 @@ CREATE TABLE comments (
     comment_text TEXT NOT NULL,
     user_id BIGINT NOT NULL,
     prototype_id BIGINT NOT NULL,
-    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (prototype_id) REFERENCES prototypes(id)
 );
